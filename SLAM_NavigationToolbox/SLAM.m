458fbedc9db61d5692a584c6c438ffb67997b1e9
Sooraj Sunil <soorajpro@gmail.com>
2022-07-16 13:02:59 -0400

    Updated SLAM.m

--- SLAM_NavigationToolbox/SLAM.m
+++ SLAM_NavigationToolbox/SLAM.m
@@ -1,9 +1,11 @@
 % 2D Laser SLAM using MATLAB  (scans collected using RPLIDAR)
 clc; clear; close all;
 
+savePlot = false; 
+
 % Load one of the 2D lidar scans
 addpath("../data/")
-fileName = 'scans_b.mat'; % {scans_a.mat, scans_b.mat}
+fileName = 'scans_a.mat'; % {scans_a.mat, scans_b.mat}
 load(fileName);
 
 % Downsample scans for efficiency
@@ -32,51 +34,43 @@ SlamAlg.LoopClosureSearchRadius = 6;
 % For recording run-time
 toc_per_sample  = zeros(1,nSamples);
 toc_total       = 0;
-map_per_sample  = cell(1,nSamples);
+GridMap  = cell(1,nSamples);
 
  f = figure('rend','painters','pos',[100 100 800 600]); clf;
 for i=1:nSamples
     oneTic = tic;
     [isScanAccepted, loopClosureInfo, optimizationInfo] = addScan(SlamAlg, samples{i});
-    [scans, optimizedPoses]  = scansAndPoses(SlamAlg);
+    [scans, poses]  = scansAndPoses(SlamAlg);
     % Build occupancy grid map
-    GridMap = buildMap(scans, optimizedPoses,mapResolution, maxLidarRange);
+    GridMap{i} = buildMap(scans, poses, mapResolution, maxLidarRange);
     % Calculate run-time
     toc_per_sample(i) = toc(oneTic);
     toc_total         = toc(oneTic)+ toc_total;
 
     % Plot figures
-    show(GridMap); hold on;
+    show(GridMap{i}); hold on;
     show(SlamAlg.PoseGraph, 'IDs', 'off');
     title('Occupancy Grid Map')
     drawnow
-    frame = getframe(f); % capture frame for file-writing
-    im = frame2im(frame);
-    [imind,cm] = rgb2ind(im,256);
-    if i==1
-          imwrite(imind,cm,'anime.gif','gif', 'Loopcount',inf, 'DelayTime', .05); 
-    else
-          imwrite(imind,cm,'anime.gif','gif','WriteMode','append', 'DelayTime', .05); 
+    if savePlot 
+        save_gif(f, i)
     end 
-
     if isScanAccepted
         fprintf('Added scan %d / %d \n', i, nSamples);
     else
         continue;
     end
-    map_per_sample{i} = GridMap; % save maps for external purposes
 end
 clear i
 
-% Save data for external purposes
-SlamInfo.fileName           = fileName;
-SlamInfo.downSampleStepSize = stepSize;
-SlamInfo.nSamples           = length(sampleIdx);
-SlamInfo.ranges             = ranges;
-SlamInfo.angles             = angles;
-SlamInfo.slamObj            = SlamAlg;
-SlamInfo.time_per_sample    = toc_per_sample;
-SlamInfo.totalTime          = toc_total;
-SlamInfo.Maps_per_sample    = map_per_sample;
-
+function save_gif(fig, i)
+frame = getframe(fig); % capture frame for file-writing
+im = frame2im(frame);
+[imind,cm] = rgb2ind(im,256);
+if i==1
+    imwrite(imind,cm,'anime.gif','gif', 'Loopcount',inf, 'DelayTime', .05);
+else
+    imwrite(imind,cm,'anime.gif','gif','WriteMode','append', 'DelayTime', .05);
+end
+end
 
